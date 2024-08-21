b2ed25be9c82061cce6265650d319798f23001b0
LoganPB <114475709+LoganPB@users.noreply.github.com>
2024-08-21 13:56:45 +0200

    feat: update themes and allow dark and light theme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,14 +1,29 @@
 local wezterm = require("wezterm")
 
 config = wezterm.config_builder()
+
+function get_appearance()
+	if wezterm.gui then
+		return wezterm.gui.get_appearance()
+	end
+	return "Dark"
+end
+function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Sakura"
+	else
+		return "Sakura (base16)"
+	end
+end
 config = {
 	automatically_reload_config = true,
 	enable_tab_bar = false,
 	window_close_confirmation = "NeverPrompt",
 	window_decorations = "RESIZE",
 	default_cursor_style = "BlinkingBar",
-	color_scheme = "tokyonight_day",
+	color_scheme = scheme_for_appearance(get_appearance()),
 	font = wezterm.font("JetBrains Mono", { weight = "Bold" }),
 	font_size = 12.5,
 }
+
 return config
