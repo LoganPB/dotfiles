b2d0f8e4764d754f2cda2ff7252d3347ea0f0d0e
Logan <logan.tempez@gmail.com>
2026-07-17 14:23:53 +0200

    feat: reset kickstart nvim conf

--- nvim/.config/nvim/lua.old/plugins/treesitter.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
-    config = function() 
-        ensure_installed = {"lua", "javascript", "typescript"}
-        highlight = { enable = true }
-        indent = { enable = true }
-    end
-}
